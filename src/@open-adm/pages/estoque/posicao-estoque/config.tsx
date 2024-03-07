19a0032ea71d31b85e1d91e99cc8004e4fe53f30
Bruno <brunobentocaina@gmail.com>
2024-03-07 13:28:46 -0300

    fix: color na posição de estoque

--- src/@open-adm/pages/estoque/posicao-estoque/config.tsx
+++ src/@open-adm/pages/estoque/posicao-estoque/config.tsx
@@ -1,4 +1,11 @@
-import { GridColDef } from "@mui/x-data-grid";
+import { GridColDef, GridRenderCellParams } from "@mui/x-data-grid";
+import { StatusObj } from "../../pedidos/config";
+import CustomChip from 'src/@open-adm/components/chip'
+
+export const quantidade: StatusObj = {
+    0: { title: 'Em aberto', color: 'warning' },
+    1: { title: 'Entregue', color: 'success' },
+}
 
 export const columns: GridColDef[] = [
     {
@@ -11,6 +18,20 @@ export const columns: GridColDef[] = [
         flex: 0.200,
         minWidth: 200,
         field: 'quantidade',
-        headerName: 'Posição do estoque'
+        headerName: 'Posição do estoque',
+        renderCell: (params: GridRenderCellParams) => {
+            const status = quantidade[params.row.quantidade > 0 ? 1 : 0]
+
+            return (
+                <CustomChip
+                    rounded
+                    size='small'
+                    skin='light'
+                    color={status.color}
+                    label={params.row.quantidade}
+                    sx={{ '& .MuiChip-label': { textTransform: 'capitalize' } }}
+                />
+            )
+        }
     },
 ]
